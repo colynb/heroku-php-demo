16bf1ce1f56997f546d9db8a50e5a4fe328f2c5e
Colyn Brown <clbrown@godaddy.com>
2014-12-29 16:42:42 -0700

    demo

--- web/index.php
+++ web/index.php
@@ -1,20 +1,26 @@
 <?php
 
-require('../vendor/autoload.php');
+require '../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+	'monolog.logfile' => 'php://stderr',
+));
+
+// Register the Twig templating engine
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__ . '/../views',
 ));
 
 // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+$app->get('/', function () use ($app) {
+	$app['monolog']->addDebug('logging output.');
+	$name = 'Colyn';
+	return $app['twig']->render('index.html', compact('name'));
 });
 
 $app->run();
